8b6f19e5ef66f7617d39a57b56e9c09be65c9923
tamassimonds <tamassimonds@gmail.com>
2025-10-14 16:47:26 +1100

    changed to pandas readcsv

--- llm_gan/train/dataset.py
+++ llm_gan/train/dataset.py
@@ -7,7 +7,12 @@ from torch.utils.data import Dataset as TorchDataset
 class StoryDataset(TorchDataset):
     def __init__(self, csv_path: str, min_story_length: int = 100):
         self.data = pd.read_csv(csv_path)
-        self.data = self.data.dropna(subset=['title', 'genre', 'human_story'])
+        
+        # Only check for non-null title and human_story (genre can be null)
+        self.data = self.data.dropna(subset=['title', 'human_story'])
+        
+        # Fill null genres with a default value
+        self.data['genre'] = self.data['genre'].fillna('General Fiction')
         
         # Filter out human stories that are too short
         self.data = self.data[self.data['human_story'].str.len() >= min_story_length]
